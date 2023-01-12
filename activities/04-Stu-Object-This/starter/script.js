030733184ab16c3c3b50b9b5e085a43e6d5098ce
JAlexander94 <118635922+JAlexander94@users.noreply.github.com>
2023-01-12 21:02:21 +0000

    exercises completed other than the last one

---
+++ activities/04-Stu-Object-This/starter/script.js
@@ -0,0 +1,27 @@
+console.log(this);
+
+function helloThis() {
+  console.log('Inside this function, this is ' + this);
+}
+
+helloThis();
+
+var child = {
+  age: 10,
+  ageTenYears: function () {
+    console.log(this.age + 10);
+  },
+};
+
+var investor = {
+  name: 'Cash Saver',
+  investment: {
+    initialInvestment: 5000,
+    investmentGrowth: function () {
+      console.log(this.initialInvestment * 1.15);
+    },
+  },
+};
+
+child.ageTenYears();
+investor.investment.investmentGrowth();
---
+++ activities/06-Stu-Manipulative-Methods/starter/script.js
@@ -0,0 +1,16 @@
+var constellations = ["Orion", "Scorpius", "Lyra", "Ursa Major", "Ursa Minor"];
+var planets = ["Earth", "Saturn", "Mars", "Jupiter", "Uranus", "Venus"];
+var star = "polaris";
+
+// WRITE YOUR CODE BELOW
+constellations.unshift("Canis Major")
+planets.pop()
+
+let galaxy = constellations.concat(planets)
+
+let capstar = star.toUpperCase()
+
+console.log(constellations)
+console.log(planets)
+console.log(galaxy)
+console.log(capstar)
